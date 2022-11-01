714d96de289348982fc72b0f438be3bb160ff460
gintaresaali <70065528+gintaresaali@users.noreply.github.com>
2022-11-01 20:10:59 +0200

    basic nft done

---
+++ App1/nftscript.py
@@ -0,0 +1,13 @@
+from web3 import Web3;
+
+import contractDetails;
+
+w3 = Web3(Web3.EthereumTesterProvider())
+
+Contract = w3.eth.contract(abi=contractDetails.abi, bytecode=contractDetails.bytecode)
+
+print("Minting NFT")
+tx_hash = Contract.constructor().transact()
+print("TX HASH=", tx_hash,"\n")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("TX RECEIPT=", tx_receipt)
