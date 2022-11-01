90fb3701b27539ffc82c6d1827ea161c92b0b15d
dfallow <dfallow@protonmail.com>
2022-11-01 15:54:55 +0200

    save metaData.json file to IPFS

--- App1/IPFS.py
+++ App1/IPFS.py
@@ -1,28 +1,32 @@
 import ipfsApi
 import os
-import json
 
-##with open('metaData.json') as file:
-##    data = json.load(file)
-
-##print(data)
-
-##print(os.listdir())
+## Get current working directory
 dir_path = os.getcwd()
 print(dir_path)
 
-f = open(dir_path +'/App1/myfile.txt', "w")
-f.write("I have created this file new")
-
-import ipfshttpclient
-
-##client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
-
-##res =client.add(f)
-##print(res)
-
-
-api = ipfsApi.Client('127.0.0.1', 5001)
-res = api.add(f)
-print(res)
-f.close
+try:
+    ## Connecting to ipfs api
+    api = ipfsApi.Client('127.0.0.1', 5001)
+    ## setting filepath for directorty which the data is stored in
+    file_path = dir_path +'/App1/'
+    try:
+        ## User inputs filename in terminal, this will be done automatically in future
+        file_name = input("Enter file name : ")
+        try:
+            ## adds file to ipfs
+            ## new_file contains 'Name', 'Hash', and 'Size'
+            new_file = api.add(file_name)
+            try:
+                ## Save the information about the added file in own text file for reference
+                file = open("save_data_address.txt", 'w')
+                file.write(str(new_file) + "\n")
+                file.write(str(new_file['Hash']))
+            except:
+                pass
+        except:
+            pass
+    except: 
+        pass
+except ipfsApi.exceptions.ConnectionError as ce:
+    print(str(ce))
