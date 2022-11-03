6124303e68f0dd38d0b998e99cfbf797c7439782
dfallow <dfallow@protonmail.com>
2022-11-03 16:42:48 +0200

    create file always gets stored in App1/tempFiles/

--- App1/connection/connection.py
+++ App1/connection/connection.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, url_for
 import json
+import os
+
+## Returns /home/dfallow/Documents/EthereumProject
+app_one_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+print(app_one_dir)
+print("test")
 
 app = Flask(__name__)
 
@@ -10,9 +16,17 @@ def index():
 @app.route('/ProcessInfo/<string:inputInfo>', methods=['POST'])
 def ProcessInfo(inputInfo):
   inputInfo = json.loads(inputInfo)
+
+  info_object = json.dumps(inputInfo, indent=2)
+
+  with open(app_one_dir + "/tempFiles/test.json", "x") as file:
+    file.write(info_object)
+
+  print(info_object)
   print()
   print(inputInfo)
   print()
+  
   return('/')
 
 if __name__ == "__main__":
