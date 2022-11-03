5e4bdb6c100c096f5b000463de642274a53f53ad
gintaresaali <70065528+gintaresaali@users.noreply.github.com>
2022-11-03 19:52:25 +0200

    trying to mint

--- App1/deployNFT.py
+++ App1/deployNFT.py
@@ -2,27 +2,42 @@ from web3 import Web3
 from web3 import EthereumTesterProvider
 import dataNFTdetails
 import IPFSv2
+import time
+from threading import Thread
 
-## test environment
+# test environment
 w3 = Web3(EthereumTesterProvider())
 
-## check if connected successfully
+# check if connected successfully
 print(w3.isConnected())
 
-## pass abi and bytecode from data generated through Remix
-contract_compiled = w3.eth.contract(abi=dataNFTdetails.abi, bytecode=dataNFTdetails.bytecode)
+# pass abi and bytecode from data generated through Remix
+contract_compiled = w3.eth.contract(
+    abi=dataNFTdetails.abi, bytecode=dataNFTdetails.bytecode)
 
-## store file on IPFS and return it's hash and url
-##ipfs daemon has to be running in the terminal
-file_hash, file_url = IPFSv2.store_ipfs_file()
-##second value is a url that displayes the file. Currently, it's just a json file.
-print("FILE1 on IPFS url: ", file_url + "\n")
-transaction_hash = contract_compiled.constructor().transact()  ##contract deployed
+# compile contract
+transaction_hash = contract_compiled.constructor().transact()
 print("TRANSACTION HASH", transaction_hash)
 transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
 print("TRANSACTION RECEIPT", transaction_receipt)
-contract_deployed = w3.eth.contract(address=transaction_receipt["contractAddress"], abi=dataNFTdetails.abi)
-print("DataItemAdded", contract_deployed.events.DataItemAdded())
-contract_deployed.functions.addDataItem(file_hash, file_url).call()
-print("DataItemAdded2", contract_deployed.events.DataItemAdded())
 
+# get contract's address
+contract_address = transaction_receipt["contractAddress"]
+# deploy contract
+# this creates a new block
+contract_deployed = w3.eth.contract(
+    address=contract_address, abi=dataNFTdetails.abi)
+
+
+def addData():
+    print("START")
+    # add file to ipfs and retrieve file's hash and url
+    file_hash, file_url = IPFSv2.store_ipfs_file()
+    # add hash and url of the ipfs file to DataItem in the contract
+    print(contract_deployed.functions.addDataItem(file_hash, file_url).call())
+    # mint?
+    print(contract_deployed.functions.mint().call())
+    print("END")
+
+
+addData()
