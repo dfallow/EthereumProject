b3a04a4df7d8501dd97907f9943157508511ef3b
gintaresaali <70065528+gintaresaali@users.noreply.github.com>
2022-11-08 14:53:52 +0200

    contract updated

--- testing/testFile.py
+++ testing/testFile.py
@@ -3,40 +3,7 @@ import os
 from web3 import Web3
 from web3 import EthereumTesterProvider
 import contractDetails
-##import IPFSv2
-import time
-from threading import Thread
-##from solcx import compile_standard, install_solc
-# install_solc("0.8.4")
 
-
-# current_dir = os.path.dirname(os.path.realpath(__file__))
-# contract_path = current_dir + '/dataNFT.sol'
-
-# with open(contract_path, 'r') as file:
-#     contract_file = file.read()
-#     print(contract_file)
-
-# print(current_dir)
-
-# compiled_solidity = compile_standard({
-#     "language": "Solidity",
-#     "sources": { "dataNFT.sol": {
-#         "content": contract_file
-#     }},
-#     "settings": {
-#         "outputSelection": {
-#             "*": { "*":
-#                 ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
-#         }
-#     }
-# }, solc_version="0.7.0", allow_paths= ".")
-
-# print("COMPILED SOLIDITY", compiled_solidity)
-
-
-ganache_url = 'http://127.0.0.1:7545'
-#w3 = Web3(Web3.HTTPProvider(ganache_url))
 w3 = Web3(Web3.EthereumTesterProvider())
 
 print(w3.isConnected())
@@ -47,6 +14,7 @@ w3.eth.default_account = w3.eth.accounts[0]
 #compile contract
 contract_compiled = w3.eth.contract(
     abi=contractDetails.abi, bytecode=contractDetails.bytecode)
+
 
 transaction_hash = contract_compiled.constructor("test hash", "test url").transact()
 print("TRANSACTION HASH", transaction_hash)
@@ -62,26 +30,25 @@ print("BLOCKS BEFORE", w3.eth.get_block('latest'))
 contract_deployed = w3.eth.contract(
     address=contract_address, abi=contractDetails.abi)
 
-print("BLOCK number", w3.eth.block_number)
-
+#check if data was added to dataItems array
 print(contract_deployed.functions.dataItems(0).call())
 
-#token owner before
-print("TOKEN OWNER before", contract_deployed.functions.getOwnerOfToken(1).call())
+#NFT owner before
+print("NFT OWNER before", contract_deployed.functions.getOwnerOfToken(1).call())
 
-#change token owner
+#change NFT owner
 contract_deployed.functions.changeOwnerOfToken(w3.eth.accounts[4], 1).transact()
 
-#owner of token after
-print("TOKEN OWNER after", contract_deployed.functions.getOwnerOfToken(1).call())
+#owner of NFT after
+print("NFT OWNER after", contract_deployed.functions.getOwnerOfToken(1).call())
 
 #contract owner
-print(contract_deployed.functions.owner().call())
+print("CONTRACT OWNER", contract_deployed.functions.owner().call())
 
 #change contract owner
-print(contract_deployed.functions.transferOwnership(w3.eth.accounts[4]).transact())
+contract_deployed.functions.transferOwnership(w3.eth.accounts[4]).transact()
 
 #contract owner
-print(contract_deployed.functions.owner().call())
+print("CONTRACT OWNER",contract_deployed.functions.owner().call())
 
 print("BLOCK number", w3.eth.block_number)
