1bf5aa8a86db496037b8ab77acfb8514efa795e8
JulianCCY <julianchancy@gmail.com>
2022-11-08 17:35:39 +0200

    added python flask in app2

---
+++ App2/app2.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__,template_folder="html")
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
