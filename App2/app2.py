3322ac2792c7254749254cb8dafa7ee147bc693e
JulianCCY <julianchancy@gmail.com>
2022-11-09 18:15:52 +0200

    created a very simply form in flask

--- App2/app2.py
+++ App2/app2.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__,template_folder="html")
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+  if request.method == "POST":
+    nft = request.form.get("nft")
+    recipient = request.form.get("recipient")
+    key = request.form.get("key")
+    
+    if not nft or not recipient or not key:
+      error = "all form fields required..."
+      return error
+    print(f"key: {key}, recipient: {recipient}, nft: {nft}")
+  return render_template("home.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
