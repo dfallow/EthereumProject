998be45ffefa043b0989433fe47e8213fe2634d0
dfallow <dfallow@protonmail.com>
2022-11-11 14:22:07 +0200

    installed black and auto indented

--- App1/deployNFT.py
+++ App1/deployNFT.py
@@ -2,8 +2,6 @@ from web3 import Web3
 from web3 import EthereumTesterProvider
 import contractDetails
 import IPFSv2
-import json
-import os
 from web3 import Web3
 from web3 import EthereumTesterProvider
 import contractDetails
@@ -15,6 +13,7 @@ w3 = Web3(EthereumTesterProvider())
 def is_connected(w3):
     return w3.isConnected()
 
+
 def deploy_nft(file_name, info_object):
     # store file on IPFS and return it's hash and url
     # ipfs daemon has to be running in the terminal
@@ -25,10 +24,10 @@ def deploy_nft(file_name, info_object):
 
     # compile contract
     contract_compiled = w3.eth.contract(
-        abi=contractDetails.abi, bytecode=contractDetails.bytecode)
+        abi=contractDetails.abi, bytecode=contractDetails.bytecode
+    )
 
-    transaction_hash = contract_compiled.constructor(
-        file_hash, file_url).transact()
+    transaction_hash = contract_compiled.constructor(file_hash, file_url).transact()
     print("TRANSACTION HASH", transaction_hash)
     transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
     print("TRANSACTION RECEIPT", transaction_receipt)
@@ -36,11 +35,12 @@ def deploy_nft(file_name, info_object):
     # retrieve contract address
     contract_address = transaction_receipt["contractAddress"]
 
-    print("BLOCKS BEFORE", w3.eth.get_block('latest'))
+    print("BLOCKS BEFORE", w3.eth.get_block("latest"))
 
     # deploy contract (creates an instance of a contract) with the address above
     contract_deployed = w3.eth.contract(
-        address=contract_address, abi=contractDetails.abi)
+        address=contract_address, abi=contractDetails.abi
+    )
 
     # check if data was added to dataItems array
     print(contract_deployed.functions.dataItems(0).call())
@@ -51,8 +51,7 @@ def deploy_nft(file_name, info_object):
     print("NFT OWNER before", contract_deployed.functions.getOwnerOfToken(1).call())
 
     # change NFT owner
-    contract_deployed.functions.changeOwnerOfToken(
-        w3.eth.accounts[4], 1).transact()
+    contract_deployed.functions.changeOwnerOfToken(w3.eth.accounts[4], 1).transact()
 
     # owner of NFT after
     print("NFT OWNER after", contract_deployed.functions.getOwnerOfToken(1).call())
@@ -61,8 +60,7 @@ def deploy_nft(file_name, info_object):
     print("CONTRACT OWNER", contract_deployed.functions.owner().call())
 
     # change contract owner
-    contract_deployed.functions.transferOwnership(
-        w3.eth.accounts[4]).transact()
+    contract_deployed.functions.transferOwnership(w3.eth.accounts[4]).transact()
 
     # contract owner
     print("CONTRACT OWNER", contract_deployed.functions.owner().call())
