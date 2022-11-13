9d3560dc3f2e020a7294cdc3c51623421f924a4b
dfallow <dfallow@protonmail.com>
2022-11-13 09:56:23 +0200

    updated python-app.yml

---
+++ App1/test_pytest.py
@@ -0,0 +1,8 @@
+import newDeployNFT
+
+#def test_is_connected():
+#    assert deployNFT.is_connected(deployNFT.w3) == True
+
+
+#def test_default_account():
+#    deployNFT.w3.eth.default_account == deployNFT.w3.eth.accounts[0]
