a86f26925c92db046d4bf4bf2bef0010d6c7d88b
dfallow <dfallow@protonmail.com>
2022-11-29 13:01:12 +0200

    works with default account

--- dapps/registerMachine/registerMachine.py
+++ dapps/registerMachine/registerMachine.py
@@ -1,15 +1,16 @@
 import easygui
-from library import ipfs, deployContracts, newContractDetails, mintNFTs
-
 import tkinter as tk
 from tkinter import *
+from library import ipfs, deployContracts, newContractDetails, mintNFTs
 
 # root window
 root =  tk.Tk()
-root.geometry("300x150")
-root.resizable(False, False)
+root.geometry("450x250")
+#root.resizable(False, False)
 root.title('Register Machine')
 
+# store user input
+file_dir = tk.StringVar()
 
 def deploy_contract():
     contract, address = deployContracts.compile_and_deploy_contract(
@@ -18,39 +19,65 @@ def deploy_contract():
         )
     return contract, address
 
-
 ## 1. uploads machine file to ipfs
 ## 2. mints the returned hash as url
 def register_machine_v1(contract, address, machine_file_path=""):
-
-    print("FILE DIR", file_dir.get())
-
-    if machine_file_path == "":
-        machine_file_path = easygui.enterbox("Test?")
 
     machine_hash = ipfs.store_file(machine_file_path)
 
     mintNFTs.mint_nft(contract, machine_hash)
 
+    set_machine_info(machine_hash)
+
     return machine_hash, contract, address
 
 machine_contract, contract_address = deploy_contract()
 
-# store user input
-file_dir = tk.StringVar()
+# register machine frame
+resgister_machine = Frame(root)
+resgister_machine.pack(padx=10, pady=10, fill='x', expand=True)
 
 # file directory
-directory_label = Label(root, text="Enter Path to Machine File")
-directory_entry = Entry(root, textvariable=file_dir)
+directory_label = Label(resgister_machine, text="Enter Path to Machine File")
+directory_label.pack(fill='x', expand=True)
+
+directory_entry = Entry(resgister_machine, textvariable=file_dir)
+directory_entry.pack(fill='x', expand=True)
+directory_entry.focus()
 
 button = Button(
-    root, 
+    resgister_machine, 
     text='Click me', 
     command=lambda: register_machine_v1(machine_contract, contract_address, file_dir.get())
     )
+button.pack(fill='x', expand=True, pady=10)
 
-directory_label.pack(fill='x', expand=True)
-directory_entry.pack(fill='x', expand=True)
-button.pack()
+# current contract frame
+contract = Frame(root, height=20)
+contract.pack(padx=10, pady=10, fill='x')
 
+# contract label
+contract_label = Label(contract, text="Current Contract Address:")
+contract_label.pack(fill='x', expand=True)
+
+# contract address
+current_contract = Entry(contract)
+current_contract.pack(fill='x', expand=True)
+
+# registered machine
+machine_label = Label(contract, text="Registerd machine:")
+machine_label.pack(fill='x', expand=True)
+
+added_machine = Entry(contract)
+added_machine.pack(fill='x', expand=True)
+
+
+def set_contract_address(address):
+    current_contract.insert(0, address)
+
+def set_machine_info(hash):
+    added_machine.insert(0, hash)
+
+set_contract_address(contract_address)
+    
 root.mainloop()
--- library/mintNFTs.py
+++ library/mintNFTs.py
@@ -14,9 +14,10 @@ def mint_nft(deployed_contract, file_hash):
     tokenId = event[0]["args"]["nftId"]
     print("NFT ID/TokenId", tokenId)
 
-    token_uri = deployed_contract.functions.tokenIdToURI(tokenId).call()
+    #token_uri = deployed_contract.functions.tokenIdToURI(tokenId).call()
 
-    token_owner = deployed_contract.functions.tokenIdToOwner(tokenId).call()
-
-    return
+    #token_owner = deployed_contract.functions.tokenIdToOwner(tokenId).call()
+    print("FILE", file_url)
+    print("TOKEN", tokenId)
+    return tokenId
 
