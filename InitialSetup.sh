9e5eeed7d9d9ebd49c566494f0a12df3357f1665
David Fallow <55578874+dfallow@users.noreply.github.com>
2022-12-01 09:55:03 +0200

    Update InitialSetup.sh

--- InitialSetup.sh
+++ InitialSetup.sh
@@ -7,3 +7,10 @@ sudo apt install python3-pip
 sudo snap install ipfs
 
 pip install ipfs-api
+pip install "web3[tester]"
+pip install eth-tester
+pip install py-evm
+
+sudo apt-get update
+sudo apt install nodejs npm
+npm install
