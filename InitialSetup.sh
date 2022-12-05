630ab89b8464ba852b0b7add82c9f5bcfb303ccd
David Fallow <55578874+dfallow@users.noreply.github.com>
2022-12-05 17:06:08 +0200

    removed ipfs install from initialSetup script

--- InitialSetup.sh
+++ InitialSetup.sh
@@ -4,8 +4,6 @@ sudo apt-get update
 sudo apt-get install python3.6
 sudo apt install python3-pip
 
-sudo snap install ipfs
-
 pip install ipfs-api
 pip install "web3[tester]"
 pip install eth-tester
