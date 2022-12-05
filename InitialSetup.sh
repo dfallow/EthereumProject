86563a09f6575336661cb0f3783aa2bd4ac2a8c1
David Fallow <55578874+dfallow@users.noreply.github.com>
2022-12-05 14:15:07 +0200

    update setup

--- InitialSetup.sh
+++ InitialSetup.sh
@@ -10,7 +10,10 @@ pip install ipfs-api
 pip install "web3[tester]"
 pip install eth-tester
 pip install py-evm
+pip install asgiref
+pip install aioflask
 
+sudo apt-get install python3-tk
 sudo apt-get update
 sudo apt install nodejs npm
 npm install
